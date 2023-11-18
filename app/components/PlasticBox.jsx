2f4686070c701ca1ba107bb47aa0c3bde9b5ad96
aswin-codes <codicon.co@gmail.com>
2023-11-18 10:38:17 +0530

    1 HDPE

--- app/components/PlasticBox.jsx
+++ app/components/PlasticBox.jsx
@@ -2,7 +2,7 @@
 
 const PlasticBox = ({ image, type, count }) => {
     return (
-        <div className="group pb-4 bg-blue-200 text-black relative h-fit  m-4 border rounded-lg shadow-md overflow-hidden transition-transform transform-gpu duration-300 hover:scale-105">
+        <div className="group pb-4 cursor-pointer bg-blue-200 text-black relative h-fit  m-4 border rounded-lg shadow-md overflow-hidden transition-transform transform-gpu duration-300 hover:scale-105">
         <img src={image} alt={type} className="w-full object-cover h-48 mx-auto mb-2" />
         <p className="text-center font-semibold text-xl mb-1">{type}</p>
         <p className="text-center text-gray-600">Count: {count}</p>
--- app/page.js
+++ app/page.js
@@ -1,14 +1,30 @@
-
+'use client'
+import { useState } from 'react';
 import PlasticBox from './components/PlasticBox';
 import plasticData from './data/plasticData';
 
 export default function Home() {
+  const [data, setData] = useState(plasticData)
+
+  //Updates data based on its id and increment value.id is 1 for Type 1, id is 2 for type 2, etc..
+  const updateData = (id,increment) => {
+    const updated = data.map(item => {
+      if (item.id == id){
+        item.count+= increment
+      }
+      return item
+    })
+    setData(updated)   
+  }
+
+
   return (
     <div className="p-4 min-h-screen bg-blue-50">
       <h1 className="text-lg text-black sm:text-3xl font-semibold mb-4">Plastic Segregation Status</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-8  sm:px-20">
-        {plasticData.map((plastic, index) => (
+        {data.map((plastic, index) => (
           <PlasticBox
+          
             key={index}
             image={plastic.image}
             type={plastic.type}
